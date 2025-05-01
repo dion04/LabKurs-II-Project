6138a237e7ff35284d7e9c84302d7dff37fc4ea0
dion04 <dion.mecini04@gmail.com>
2025-05-01 23:31:25 +0200

    hero change

--- client/src/components/landing/Hero.tsx
+++ client/src/components/landing/Hero.tsx
@@ -11,22 +11,21 @@ export const Hero = () => {
         <main className='text-5xl md:text-6xl font-bold'>
           <h1 className='inline'>
             <span className='inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text'>
-              Shadcn
+              People's Voice
             </span>{' '}
-            landing page
+            platform
           </h1>{' '}
           for{' '}
           <h2 className='inline'>
             <span className='inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text'>
-              React
+              crowd-sourced news
             </span>{' '}
-            developers
+            and stories
           </h2>
         </main>
 
         <p className='text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0'>
-          Build your React landing page effortlessly with the required sections
-          to your project.
+          Join a platform to share stories, report events, and amplify voices.
         </p>
 
         <div className='space-y-4 md:space-y-0 md:space-x-4'>
